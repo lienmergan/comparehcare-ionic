a8a85bea2399a3b0591d20f4b35eb2f066f0fa5b
lienmergan <lien.mergan@gmail.com>
2016-05-14 18:33:43 +0200

    Make changes to procfile and server.js file

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
  * Created by lienmergan on 14/05/16.
  */
 
-var express = require('express'),
-app = express();
-app.use(express.static('app/www'));
-app.set('port', process.env.PORT || 3000);
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + "/www"));
+app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
